8334de4eddda82feae5182d3df12d3b696d05c0f
madhavreddy509 <madhavreddy00570@gmail.com>
2024-04-20 09:14:22 +0530

    change the style

--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -3,22 +3,21 @@ import React from "react";
 const Sidebar = () => {
   return (
     <div className="sidebar">
-      {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
-      <a href="/" className="active">
+      <button className="active">
         <span role="img" aria-label="Notes">&#128220;</span> Notes
-      </a>
-      <a href="/">
+      </button>
+      <button>
         <span role="img" aria-label="Reminders">&#128276;</span> Reminders
-      </a>
-      <a href="/">
+      </button>
+      <button>
         <span role="img" aria-label="Edit Labels">&#128196;</span> Edit Labels
-      </a>
-      <a href="/">
+      </button>
+      <button>
         <span role="img" aria-label="Archive">&#128451;</span> Archive
-      </a>
-      <a href="/">
+      </button>
+      <button>
         <span role="img" aria-label="Trash">&#128465;</span> Trash
-      </a>
+      </button>
     </div>
   );
 };
